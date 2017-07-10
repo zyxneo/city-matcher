c95787e71cf8e4de6258d2a734da9c90dfcd23ba
Szilágyi Balázs <zyxneo@gmail.com>
2017-07-10 14:13:15 +0200

    css grid solution

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Autocomplete from "nukleus/dist/components/Autocomplete";
 
 import grid from "./flexSolution.scss";
+// import grid from "./gridSolution.scss"; // alternative grid solution
 import style from "./homePage.scss";
 
 export default class HomePage extends React.Component {
@@ -138,6 +139,7 @@ export default class HomePage extends React.Component {
                 ${grid.grid__col_12}
                 ${grid.grid__col_sm_6}
                 ${grid.grid__col_md_4}
+                ${grid.grid_align_center}
                 ${grid.grid_order_1}
                 ${grid.grid_order_2_sm}
               `}>
